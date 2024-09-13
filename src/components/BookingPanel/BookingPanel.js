417faff05ea7e3851ed84192249f4fa621c6ff2e
giodong16 <hoangnam14022003@gmail.com>
2024-09-14 00:24:58 +0700

    pagination, bookingpanel, registration form

---
+++ src/components/BookingPanel/BookingPanel.js
@@ -0,0 +1,70 @@
+import { IoTicketOutline } from 'react-icons/io5'; 
+import { PiMapPinAreaLight } from "react-icons/pi";
+import { FaRegClock } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { LuCalendarDays } from "react-icons/lu";
+
+const BookingPanel = () => {
+
+    return (
+        <div className="md:w-11/12  mx-auto md:flex ">
+            {/* Phần bên trái */}
+            <div className="md:w-2/3 border border-red-500 ">
+                Left
+                <div className='h-96'>1</div>
+                <div className='h-96'>1</div>
+                <div className='h-96'>1</div>
+                <div className='h-96'>1</div>
+                <div className='h-96'>1</div>
+                <div className='h-96'>1</div>
+                <div className='h-96'>1</div>
+            </div>
+            
+            {/* Phần bên phải */}
+            <div className="w-full max-w-[350px] mx-auto md:max-w-none md:w-1/3 flex flex-col p-4 sticky right-0 top-20 self-start ">
+
+
+                <div className="animate-slideInUp shadow-xl hover:shadow-2xl transition-shadow duration-300 p-6 rounded-lg">
+                    <div className="mb-4">
+                        <h4 className="text-lg font-semibold">Giá:</h4>
+                        <p className="sm:text-3xl text-xl  font-bold text-red-600">690,000 đ <span className="text-sm">/ Khách</span></p>
+                    </div>
+
+                    <div className="space-y-4">
+                        <div className="flex items-center sm:text-base text-sm ">
+                            <IoTicketOutline className='w-5 h-5 mr-2' />
+                            <p>Mã tour: <span className="font-bold">NDCTH804-003-210924TAU-H</span></p>
+                        </div>
+
+                        <div className="flex items-center sm:text-base text-sm ">
+                            <PiMapPinAreaLight className='w-5 h-5 mr-2' />
+                            <p>Khởi hành: <span className="font-bold">Cần Thơ</span></p>
+                        </div>
+
+                        <div className="flex items-center sm:text-base text-sm ">
+                            <LuCalendarDays className='w-5 h-5 mr-2' />
+                            <p>Ngày khởi hành: <span className="font-bold">21-09-2024</span></p>
+                        </div>
+
+                        <div className="flex items-center sm:text-base text-sm ">
+                            <FaRegClock className='w-5 h-5 mr-2' />
+                            <p>Thời gian: <span className="font-bold">Trong ngày</span></p>
+                        </div>
+
+                        <div className="flex items-center sm:text-base text-sm ">
+                            <FiUsers className='w-5 h-5 mr-2' />
+                            <p>Số chỗ còn: <span className="font-bold">6 chỗ</span></p>
+                        </div>
+                    </div>
+
+                    <div className="flex space-x-4 mt-6">
+                        <button className="bg-white border  sm:text-base text-sm border-red-500 text-red-500 sm:rounded-lg rounded-md px-2 py-1 sm:px-4 sm:py-2 w-1/3" type="button" aria-label="Ngày khác">Ngày khác</button>
+                        <button className="bg-red-500  sm:text-base text-sm text-white sm:rounded-lg rounded-md hover:bg-red-600  px-2 py-1 sm:px-4 sm:py-2 w-2/3" type="button" aria-label="Đặt tour">Đặt tour</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default BookingPanel;
---
+++ src/components/Form/RegistrationForm.js
@@ -0,0 +1,152 @@
+import { FaUser } from "react-icons/fa";
+import { PiGenderIntersexBold } from "react-icons/pi";
+import { FaPhoneAlt } from "react-icons/fa";
+import { FaHouseUser } from "react-icons/fa";
+import { BsCalendar2Day } from "react-icons/bs";
+import { FaRegIdCard } from "react-icons/fa";
+import { IoMdMail } from "react-icons/io";
+import { FaLock } from "react-icons/fa";
+import { FaEye,FaEyeSlash  } from "react-icons/fa";
+const RegistrationForm = () => {
+
+    return (
+        <div className="container mx-auto">
+        <div className="flex justify-center items-center h-screen">
+          <div className="lg:w-2/3 md:w-5/6 md:max-w-[850px] mx-auto max-w-none px-4">
+            <div className="border bg-white p-6 rounded-lg shadow-md">
+              <div className="mb-4">
+                <h3 className="text-2xl font-bold text-center text-red-500">ĐĂNG KÍ</h3>
+              </div>
+              <div className="mb-4">
+                <form action="#" method="POST">
+                  <div className="space-y-4">
+                    <div className="flex flex-wrap -mx-3">
+                      <div className="w-full md:w-1/2 px-3 mb-4">
+                        <div className="relative ">
+                          
+                          <div className="absolute inset-y-0 left-0 pl-3 flex items-center ">
+                                 <FaUser className="h-4 w-4 text-blue-500" />
+                          </div>
+                          <div className="border rounded-md border-gray-400 overflow-hidden">
+                          <input type="text" className="pl-10 py-2 border-none outline-none  w-full" placeholder="Họ tên" required/>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="w-full md:w-1/2 px-3 mb-4">
+                        <div className="relative">
+                          <div className="absolute inset-y-0 left-0 pl-3 flex items-center ">
+                                <BsCalendar2Day className="h-4 w-4 text-blue-500" />
+                          </div>
+                          <div className="border rounded-md border-gray-400 overflow-hidden">
+                          <input type="text" className="pl-10 py-2 border-none outline-none w-full" placeholder="Ngày sinh"/>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="w-full md:w-1/2 px-3 mb-4">
+                        <div className="relative">
+                          <div className="absolute inset-y-0 left-0 pl-3 flex items-center ">
+                                 <PiGenderIntersexBold className="h-5 w-5 text-blue-500" />
+                          </div>
+                          <div className="border rounded-md border-gray-400 overflow-hidden">
+                          <input type="text" className="pl-10 py-2 border-none outline-none w-full" placeholder="Giới tính" required/>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="w-full md:w-1/2 px-3 mb-4">
+                        <div className="relative">
+                          <div className="absolute inset-y-0 left-0 pl-3 flex items-center ">
+                                 <FaRegIdCard className="h-4 w-4 text-blue-500" />
+                          </div>
+                          <div className="border rounded-md border-gray-400 overflow-hidden">
+                          <input type="text" className="pl-10 py-2 border-none outline-none w-full" placeholder="CCCD" required/>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="w-full md:w-1/2 px-3 mb-4">
+                        <div className="relative">
+                          <div className="absolute inset-y-0 left-0 pl-3 flex items-center ">
+                                 <FaPhoneAlt className="h-4 w-4 text-blue-500" />
+                          </div>
+                          <div className="border rounded-md border-gray-400 overflow-hidden">
+                          <input type="text" className="pl-10 py-2 border-none outline-none w-full" placeholder="Di động" required/>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="w-full md:w-1/2 px-3 mb-4">
+                        <div className="relative">
+                          <div className="absolute inset-y-0 left-0 pl-3 flex items-center ">
+                                 <IoMdMail className="h-4 w-4 text-blue-500" />
+                          </div>
+                          <div className="border rounded-md border-gray-400 overflow-hidden">
+                          <input type="text" className="pl-10 py-2 border-none outline-none w-full" placeholder="Email" required/>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="w-full px-3 mb-4">
+                        <div className="relative">
+                          <div className="absolute inset-y-0 left-0 pl-3 flex items-center ">
+                                 <FaHouseUser className="h-4 w-4 text-blue-500" />
+                          </div>
+                          <div className="border rounded-md border-gray-400 overflow-hidden">
+                          <input type="text" className="pl-10 py-2 border-none outline-none w-full" placeholder="Địa chỉ" required/>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="w-full md:w-1/2 px-3 mb-4">
+                        <div className="relative">
+                          <div className="absolute inset-y-0 left-0 pl-3 flex items-center ">
+                                 <FaLock className="h-4 w-4 text-blue-500" />
+                          </div>
+                          <div className="border rounded-md border-gray-400 overflow-hidden">
+                          <input type="password" className="pl-10 py-2 border-none outline-none w-full" placeholder="Mật khẩu" required/>
+                          </div>
+                          <div className="absolute inset-y-0 right-0 pr-3 flex items-center ">
+                                 <FaEye className="h-4 w-4 text-blue-500" />
+                          </div>
+                        </div>
+                      </div>
+                      <div className="w-full md:w-1/2 px-3 mb-4">
+                        <div className="relative">
+                          <div className="absolute inset-y-0 left-0 pl-3 flex items-center ">
+                                 <FaLock className="h-4 w-4 text-blue-500" />
+                          </div>
+                          <div className="border rounded-md border-gray-400 overflow-hidden">
+                          <input type="password" className="pl-10 py-2 border-none outline-none w-full" placeholder="Xác nhận mật khẩu" required/>
+                          </div>
+                          <div className="absolute inset-y-0 right-0 pr-3 flex items-center ">
+                                 <FaEyeSlash  className="h-4 w-4 text-blue-500" />
+                          </div>
+                        </div>
+                      </div>
+                      <div className="w-full px-3 mb-4">
+                        <label className="inline-flex items-center">
+                          <input type="checkbox" name="rule" id="rule" className="form-checkbox"/>
+                          <span className="ml-2 text-sm text-gray-600">Tôi đã đọc và đồng ý các điều khoản bên dưới</span>
+                        </label>
+                      </div>
+                    </div>
+                    <div className=" md:max-w-[250px] mx-auto max-w-none">
+                        <button type="submit" className="bg-red-500 text-white w-full py-2 rounded-full hover:bg-red-600 transition">Đăng kí</button>
+                    </div>
+                  </div>
+                </form>
+              </div>
+              <div className="text-center  py-4 border-t relative ">
+                <a href="https://www.facebook.com" target="_blank" className="inline-block px-3">
+                  <img src="https://www.dpreview.com/files/p/articles/4698742202/facebook.jpeg" alt="Facebook" className="w-8 h-8 p-0 rounded-full"/>
+                </a>
+                <a href="https://www.google.com" target="_blank" className="inline-block px-3">
+                  <img src="https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-suite-everything-you-need-know-about-google-newest-0.png" alt="Google" className="w-8 h-8 p-0 rounded-full"/>
+                </a>
+                <div className="absolute top-[-13px] left-1/2 transform -translate-x-1/2 bg-white">or connect with</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      
+
+    );
+} ;
+
+export default RegistrationForm
