183de392c9c5d642c60b2e83ac05243614160a96
giodong16 <hoangnam14022003@gmail.com>
2024-10-07 12:05:03 +0700

    update tí ẻm tourdetail, ai có thẩm mĩ làm đẹp đi, t làm logic qua qua

--- src/components/BookingPanel/BookingPanel.js
+++ src/components/BookingPanel/BookingPanel.js
@@ -5,54 +5,45 @@ import { FiUsers } from "react-icons/fi";
 import { LuCalendarDays } from "react-icons/lu";
 import { useNavigate } from 'react-router-dom';
 
-const BookingPanel = () => {
+const BookingPanel = () => { 
+    const navigate = useNavigate(); 
+    return ( 
+        <div className="w-full mx-auto lg:max-w-none lg:w-1/3 lg:sticky right-0 lg:top-20 lg:self-start fixed z-30 lg:z-0 bottom-0 left-0 bg-white shadow-lg shadow-opacity-50 lg:shadow-none border-t-2 lg:border-t-0 border-red-400 ">
+            <div className="lg:animate-slideInUp lg:shadow-xl hover:shadow-2xl lg:transition-shadow  p-6 rounded-lg shadow-md transition-shadow duration-300">
+                <div className="mb-4">
+                    <h4 className="text-lg font-semibold">Giá:</h4> 
+                    <p className="sm:text-3xl text-xl font-bold text-red-600">690,000 đ <span className="text-sm">/ Khách</span></p>
+                </div>
 
-    const navigate = useNavigate()
-    return (
-       
-           
-            <div className="w-full max-w-[350px] mx-auto md:max-w-none md:w-1/3 flex flex-col sticky right-0 top-20 self-start ">
-
-
-                <div className="animate-slideInUp shadow-xl hover:shadow-2xl transition-shadow duration-300 p-6 rounded-lg">
-                    <div className="mb-4">
-                        <h4 className="text-lg font-semibold">Giá:</h4>
-                        <p className="sm:text-3xl text-xl  font-bold text-red-600">690,000 đ <span className="text-sm">/ Khách</span></p>
+                <div className="space-y-4 hidden lg:block">
+                    <div className="flex items-center sm:text-base text-sm ">
+                        <IoTicketOutline className='w-5 h-5 mr-2' />
+                        <p>Mã tour: <span className="font-bold">NDCTH804-003-210924TAU-H</span></p>
                     </div>
-
-                    <div className="space-y-4">
-                        <div className="flex items-center sm:text-base text-sm ">
-                            <IoTicketOutline className='w-5 h-5 mr-2' />
-                            <p>Mã tour: <span className="font-bold">NDCTH804-003-210924TAU-H</span></p>
-                        </div>
-
-                        <div className="flex items-center sm:text-base text-sm ">
-                            <PiMapPinAreaLight className='w-5 h-5 mr-2' />
-                            <p>Khởi hành: <span className="font-bold">Cần Thơ</span></p>
-                        </div>
-
-                        <div className="flex items-center sm:text-base text-sm ">
-                            <LuCalendarDays className='w-5 h-5 mr-2' />
-                            <p>Ngày khởi hành: <span className="font-bold">21-09-2024</span></p>
-                        </div>
-
-                        <div className="flex items-center sm:text-base text-sm ">
-                            <FaRegClock className='w-5 h-5 mr-2' />
-                            <p>Thời gian: <span className="font-bold">Trong ngày</span></p>
-                        </div>
-
-                        <div className="flex items-center sm:text-base text-sm ">
-                            <FiUsers className='w-5 h-5 mr-2' />
-                            <p>Số chỗ còn: <span className="font-bold">6 chỗ</span></p>
-                        </div>
+                    <div className="flex items-center sm:text-base text-sm ">
+                        <PiMapPinAreaLight className='w-5 h-5 mr-2' />
+                        <p>Khởi hành: <span className="font-bold">Cần Thơ</span></p>
                     </div>
-
-                    <div className="flex space-x-4 mt-6">
-                        <button className="bg-white border  sm:text-base text-sm border-red-500 text-red-500 sm:rounded-lg rounded-md px-2 py-1 sm:px-4 sm:py-2" type="button" aria-label="Ngày khác">Ngày khác</button>
-                        <button onClick={() => navigate('/booking/example')} className="bg-red-500  sm:text-base text-sm text-white sm:rounded-lg rounded-md hover:bg-red-600  px-2 py-1 sm:px-4 sm:py-2" type="button" aria-label="Đặt tour">Đặt tour</button>
+                    <div className="flex items-center sm:text-base text-sm ">
+                        <LuCalendarDays className='w-5 h-5 mr-2' />
+                        <p>Ngày khởi hành: <span className="font-bold">21-09-2024</span></p>
+                    </div>
+                    <div className="flex items-center sm:text-base text-sm ">
+                        <FaRegClock className='w-5 h-5 mr-2' />
+                        <p>Thời gian: <span className="font-bold">Trong ngày</span></p>
+                    </div>
+                    <div className="flex items-center sm:text-base text-sm ">
+                        <FiUsers className='w-5 h-5 mr-2' />
+                        <p>Số chỗ còn: <span className="font-bold">6 chỗ</span></p>
                     </div>
                 </div>
+
+                <div className="flex space-x-4 mt-6">
+                    <button className="bg-white border sm:text-base text-sm border-red-500 text-red-500 sm:rounded-lg rounded-md px-2 py-1 sm:px-4 sm:py-2" type="button" aria-label="Ngày khác">Ngày khác</button>
+                    <button onClick={() => navigate('/booking/example')} className="bg-red-500 sm:text-base text-sm text-white sm:rounded-lg rounded-md hover:bg-red-600 px-2 py-1 sm:px-4 sm:py-2" type="button" aria-label="Đặt tour">Đặt tour</button>
+                </div>
             </div>
+        </div>
     );
 };
 
--- src/pages/Tour/TourDetail.js
+++ src/pages/Tour/TourDetail.js
@@ -6,15 +6,16 @@ import Ltinerary from "../../components/Ltinerary/Ltinerary"
 const TourDetail = () => {
     return (
         <div className="flex justify-center flex-col">
-            <h1 className="font-bold text-3xl py-4">Dịch Vụ Tham Quan Trải Nghiệm Xe Bus 2 Tầng & Ăn Tối Trên Tàu Indochina</h1>
-            <div className="flex">
-                <div className="w-2/3 mr-8">
+            <h1 className="font-bold lg:text-3xl text-2xl py-4">Dịch Vụ Tham Quan Trải Nghiệm Xe Bus 2 Tầng & Ăn Tối Trên Tàu Indochina</h1>
+            <div className="lg:flex">
+                <div className="lg:w-2/3 lg:mr-8">
                     <Slider />
                     <h1 className="text-center font-bold text-3xl py-4">Lịch khởi hành</h1>
                     <h1 className="text-center font-bold text-3xl py-4">Lịch trình</h1>
                     <Ltinerary/>
                 </div>
-                <BookingPanel />
+                <BookingPanel/>
+                
             </div>
         </div>
     )
